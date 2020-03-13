d03d754e57ec5234a4cfbc0573643ad000484bd1
jona <jona@jona.com>
2020-03-13 23:55:47 +0300

    added the templating engine

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-const express= require('express');
+const express =require('express');
+const handlebars=require('express3-handlebars').create({defaultLayout:'main'});
 
 
-app=express();
-app.set('port',process.env.PORT||3000);
+const app=express();
+
+
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+app.set('port',process.env.PORT ||3000);
+
 
 app.get('/',(req,res)=>{
-    res.send("Home Page")
+    res.send('Home Page')
 });
 
 app.get('/about/',(req,res)=>{
-   res.send("About Page")
+  res.send("ABout Page")
 });
 
-
 app.use((req,res)=>{
+    res.status(400)
+    res.send("404 Not Found")
     res.type('text/plain')
-    res.status(404)
-    res.send('404 Not Found')
-})
+
+});
 
 app.use((req,res)=>{
     res.status(500)
-    res.type('text/plain')
-    res.send("500 Internal server Error")
+    res.send("500 Internal Server Error")
+    res.type("text/plain")
+});
 
-})
-
-app.listen(app.get('port'),(req,res)=>{
-    console.log("Server running on port " + app.get('port'));
-    
-})
+app.listen(app.get('port'),()=>{
+    console.log("Server is running at http://localhost:" +app.get('port')+ " Press Ctrl C to Terminate");
+});
