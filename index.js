19b732635e21ee05bd3129bd9a883714da6e3a96
jona <jona@jona.com>
2020-03-13 23:22:53 +0300

    created some routes

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-const express=require('express'); //bring in expess
+const express= require('express');
 
-const app =express(); //make an express 
 
+app=express();
 app.set('port',process.env.PORT||3000);
+
 app.get('/',(req,res)=>{
-    res.type('text/plain')
     res.send("Home Page")
 });
 
-app.get('about/',(req,res)=>{
-    res.type('text/plain')
-    res.send("About Page")
+app.get('/about/',(req,res)=>{
+   res.send("About Page")
 });
+
 
 app.use((req,res)=>{
     res.type('text/plain')
     res.status(404)
-    res.send('404 - NOT FOUND')
-});
+    res.send('404 Not Found')
+})
 
 app.use((req,res)=>{
+    res.status(500)
     res.type('text/plain')
-    res.status(404)
-    res.send('500 -SERVER ERROR')
-});
+    res.send("500 Internal server Error")
 
-app.listen(app.get('port'),()=>{
-    console.log("Express started on http://localhost:" + app.get('port') 
-     + "press Ctrl + C to terminate"
-    );
+})
+
+app.listen(app.get('port'),(req,res)=>{
+    console.log("Server running on port " + app.get('port'));
     
-}
-);
+})
