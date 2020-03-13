4ddea84650c1493b780b69e1386bf7a35b4b3401
jona <jona@jona.com>
2020-03-13 15:02:43 +0300

    started project

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require('express'); //bring in expess
+
+const app =express(); //make an express 
+
+app.set('port',process.env.PORT||3000);
+
+app.use((req,res)=>{
+    res.type('text/plain')
+    res.status(404)
+    res.send('404 - NOT FOUND')
+});
+
+app.use((req,res)=>{
+    res.type('text/plain')
+    res.status(404)
+    res.send('500 -SERVER ERROR')
+});
+
+app.listen(app.get('port'),()=>{
+    console.log("Express started on http://localhost:" + app.get('port') 
+     + "press Ctrl + C to terminate"
+    );
+    
+}
+);
